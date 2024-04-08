98aefabef1d4621ff5262f7309b96491362d8629
Ilja Semencenko <ilja.semencenko@swisscom.com>
2024-04-08 21:15:51 +0300

    ami-1.2

---
+++ src/app/home/disclaimer-text/discalimer-text.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'disclaimer-text',
+  templateUrl: './disclaimer-text.html',
+  styleUrls: ['./disclaimer-text.scss'],
+})
+export class DisclaimerText {}
